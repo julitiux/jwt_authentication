255a219f91ec607cd41e4ce65ca1d826185704bb
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-10 20:10:04 -0600

    using enum Role into class User with EnumType.STRING

--- src/main/java/com/jwt_authentication/User.java
+++ src/main/java/com/jwt_authentication/User.java
@@ -1,9 +1,6 @@
 package com.jwt_authentication;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,6 +25,9 @@ public class User implements UserDetails {
   private String lastname;
   private String email;
   private String password;
+
+  @Enumerated(EnumType.STRING)
+  private Role role;
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
