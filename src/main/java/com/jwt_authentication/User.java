c44923a49e5ea697e1977cf035ad58e449cfb9a6
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-10 19:38:51 -0600

    add properties tothe class User

--- src/main/java/com/jwt_authentication/User.java
+++ src/main/java/com/jwt_authentication/User.java
@@ -1,4 +1,11 @@
 package com.jwt_authentication;
 
 public class User {
+
+  private Integer id;
+  private String firstname;
+  private String lastname;
+  private String email;
+  private String password;
+
 }
