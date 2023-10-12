c357713ea3d027d4193fe1b203689e9b1e404cc8
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-12 12:52:19 -0600

    declare a variable and todo, because we need to extract the userEmail from JWT token

--- src/main/java/com/jwt_authentication/config/JwtAuthenticationFilter.java
+++ src/main/java/com/jwt_authentication/config/JwtAuthenticationFilter.java
@@ -23,10 +23,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     final String authHeader = request.getHeader("Authorization");
     final String jwt;
+    final String userEmail;
     if (authHeader == null || !authHeader.startsWith("Bearer ")) {
       filterChain.doFilter(request, response);
       return;
     }
     jwt = authHeader.substring(7);
+    userEmail = // todo:extract the userEmail from JWT token
   }
 }
