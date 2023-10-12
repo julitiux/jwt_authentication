e8389f93fae3fa66120de8375616922dd7dec084
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-12 12:39:33 -0600

    get the headers from HttpServletRequest request

--- src/main/java/com/jwt_authentication/config/JwtAuthenticationFilter.java
+++ src/main/java/com/jwt_authentication/config/JwtAuthenticationFilter.java
@@ -19,9 +19,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   protected void doFilterInternal(
     @NonNull HttpServletRequest request,
     @NonNull HttpServletResponse response,
-    @NonNull FilterChain filterChain)
+    @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-    throws ServletException, IOException {
-
+    final String authHeader = request.getHeader("Authorization");
   }
 }
