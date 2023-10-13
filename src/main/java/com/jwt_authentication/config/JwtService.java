89a03ef7c1425ade805b0af7f3d4439af60d73a0
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-13 11:23:47 -0600

    start implement method extractAllClaims(String token) and return Claims

--- src/main/java/com/jwt_authentication/config/JwtService.java
+++ src/main/java/com/jwt_authentication/config/JwtService.java
@@ -1,9 +1,17 @@
 package com.jwt_authentication.config;
 
+import io.jsonwebtoken.Claims;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+
   public String extractUsername(String jwt) {
+    return null;
   }
+
+  private Claims extractAllClaims(String token) {
+    return null;
+  }
+
 }
