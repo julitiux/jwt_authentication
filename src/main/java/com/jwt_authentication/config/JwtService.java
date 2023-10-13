ee696899ce7b1b906b3ff5a5e19a2d477dc6735f
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-13 11:50:08 -0600

    implement method getSignInKey()

--- src/main/java/com/jwt_authentication/config/JwtService.java
+++ src/main/java/com/jwt_authentication/config/JwtService.java
@@ -2,6 +2,8 @@ package com.jwt_authentication.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -9,7 +11,7 @@ import java.security.Key;
 @Service
 public class JwtService {
 
-  private static final String SECRET_KEY="";
+  private static final String SECRET_KEY = "";
 
   public String extractUsername(String jwt) {
     return null;
@@ -25,7 +27,8 @@ public class JwtService {
   }
 
   private Key getSignInKey() {
-    return null;
+    byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+    return Keys.hmacShaKeyFor(keyBytes);
   }
 
 }
