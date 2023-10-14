e5291a3c7f65f573be31dc4f75d6f52c53761ea5
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-14 17:20:11 -0600

    declare method isTokenExpired; recibe params like String token and return a boolean

--- src/main/java/com/jwt_authentication/config/JwtService.java
+++ src/main/java/com/jwt_authentication/config/JwtService.java
@@ -45,7 +45,11 @@ public class JwtService {
 
   public boolean isTokenValid(String token, UserDetails userDetails) {
     final String username = extractUsername(token);
-    return (username.equals(userDetails.getUsername())) && !isTokenExpired(token); // TODO: implement isTokenExpired
+    return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+  }
+
+  private boolean isTokenExpired(String token) {
+    return false;
   }
 
   private Claims extractAllClaims(String token) {
