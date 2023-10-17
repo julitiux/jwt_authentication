8c688d34a3050edcb742af12dfa631b06d3b43f9
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-16 22:05:17 -0600

    add tag @RequiredArgsConstructor by Lombok

--- src/main/java/com/jwt_authentication/config/ApplicationConfig.java
+++ src/main/java/com/jwt_authentication/config/ApplicationConfig.java
@@ -1,7 +1,9 @@
 package com.jwt_authentication.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
 }
