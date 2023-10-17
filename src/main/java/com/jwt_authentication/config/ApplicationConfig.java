5403ebb55d4fbc56b7126727940c3e71d625fe0b
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-16 22:46:09 -0600

    inject UserRepository and using the bean userRepository.findByEmail into the method userDetailsService. In some cases it could be null, launch a exception UsernameNotFoundException with the string 'User not found'

--- src/main/java/com/jwt_authentication/config/ApplicationConfig.java
+++ src/main/java/com/jwt_authentication/config/ApplicationConfig.java
@@ -1,17 +1,22 @@
 package com.jwt_authentication.config;
 
+import com.jwt_authentication.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
+  private UserRepository userRepository;
+
   @Bean
   public UserDetailsService userDetailsService() {
-    return username -> null;
+    return username -> userRepository.findByEmail(username)
+      .orElseThrow(() -> new UsernameNotFoundException("User not found"));
   }
 
 }
