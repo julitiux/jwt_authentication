395cf4811820ef875e556b4481acfa93294c2217
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-16 23:09:06 -0600

    lets get the username by this.userDetailsService.loadUserByUsername, the param is the userEmail

--- src/main/java/com/jwt_authentication/config/JwtAuthenticationFilter.java
+++ src/main/java/com/jwt_authentication/config/JwtAuthenticationFilter.java
@@ -7,6 +7,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +19,8 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private JwtService jwtService;
+
+  private UserDetailsService userDetailsService;
 
   @Override
   protected void doFilterInternal(
@@ -34,6 +38,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     jwt = authHeader.substring(7);
     userEmail = jwtService.extractUsername(jwt);
     if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
     }
   }
 }
