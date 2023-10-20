c08910b73bdcbe9c626caeddf09aa42ae61a1c47
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-19 23:58:02 -0600

    we eneable web security

--- src/main/java/com/jwt_authentication/config/SecurityConfiguration.java
+++ src/main/java/com/jwt_authentication/config/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.jwt_authentication.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 }
