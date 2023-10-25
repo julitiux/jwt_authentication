41a276c3948fd3ee5f1d0c12ec480cd1e5e554c4
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-24 21:32:21 -0600

    starting to implement method that the data access object which is responsable to fetch the UserDetails and also encode password

--- src/main/java/com/jwt_authentication/config/ApplicationConfig.java
+++ src/main/java/com/jwt_authentication/config/ApplicationConfig.java
@@ -4,6 +4,8 @@ import com.jwt_authentication.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -17,6 +19,14 @@ public class ApplicationConfig {
   public UserDetailsService userDetailsService() {
     return username -> userRepository.findByEmail(username)
       .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    authenticationProvider.setUserDetailsService(userDetailsService());
+    authenticationProvider.setPasswordEncoder(passwordEncoder());  //TODO: implement method passwordEncoder()
+    return authenticationProvider;
   }
 
 }
