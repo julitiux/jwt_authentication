9daf93013837b8c0ee3afc02add594bf539af795
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-10-26 22:17:27 -0600

    starting to implement AuthenticationService as a service

---
+++ src/main/java/com/jwt_authentication/auth/AuthenticationService.java
@@ -0,0 +1,9 @@
+package com.jwt_authentication.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+}
